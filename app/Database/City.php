ff737afb1eaff9d64496eb6493f1be33bddc1b20
sonypiay <sonypiay@mail.com>
2019-11-02 02:13:08 +0700

    commit change

---
+++ app/Database/City.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Database;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+  public $timestamps = false;
+  protected $table = 'city';
+  protected $primaryKey = 'city_id';
+  public $incrementing = false;
+
+  public function getAllCity( $sort = null, $request = null )
+  {
+    $sort = $sort === null ? 'asc' : $sort;
+    if( $request === null )
+    {
+      return $this->join('province', 'city_id.province_id', '=', 'province.province_id')
+      ->orderBy('city.city_name', $sort)
+      ->get();
+    }
+    else
+    {
+      $keywords = $request->keywords;
+      $limit = $request->limit;
+
+      $query = $this->join('province', 'city_id.province_id', '=', 'province.province_id')
+      ->orderBy('city.city_name', $sort);
+
+      if( ! empty( $keywords ) )
+      {
+        $query = $query
+        ->where('city.city_name', 'like', '%' . $keywords . '%')
+        ->orWhere('province.province_name', 'like', '%' . $keywords . '%');
+      }
+
+      return $query->paginate( $limit );
+    }
+  }
+
+  public function getCity( $id )
+  {
+    return $this->join('province', 'city_id.province_id', '=', 'province.province_id')
+    ->where('city.city_id', $id)->first();
+  }
+
+  public function addCity( $request )
+  {
+    $city_name = $request->city_name;
+    $city_id = $request->city_id;
+    $province = $request->province;
+    $res = [ 'statusCode' => 200, 'errorMessage' => '' ];
+
+    $check_id = $this->where('city_id', $city_id);
+    if( $check_id->count() == 1 )
+    {
+      $res = [ 'statusCode' => 409, 'errorMessage' => 'Duplicate ID. ID already exists!' ];
+    }
+    else
+    {
+      $this->city_id = $city_id;
+      $this->city_name = $city_name;
+      $this->province_id = $province;
+      $this->save();
+    }
+
+    return $res;
+  }
+
+  public function updateCity( $request, $id )
+  {
+    $city_name = $request->city_name;
+    $city_id = $request->city_id;
+    $province = $request->province;
+    $res = [ 'statusCode' => 200, 'errorMessage' => '' ];
+
+    $getcity = $this->getCity( $id );
+    $getcity->city_name = $city_name;
+    $getcity->province_id = $province;
+
+    if( $getcity->city_id === $city_id )
+    {
+      $getcity->save();
+    }
+    else
+    {
+      $check_id = $this->where('city_id', $city_id);
+      if( $check_id->count() == 1 )
+      {
+        $res = [ 'statusCode' => 409, 'errorMessage' => 'Duplicate ID. ID already exists!' ];
+      }
+      else
+      {
+        $getcity->city_id = $city_id;
+        $getcity->save();
+      }
+    }
+  }
+
+  public function deleteCity( $id )
+  {
+    if( $id === null || empty( $id ) ) return false;
+    return $this->where('city_id', $id)->delete();
+  }
+}
---
+++ app/Database/Province.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Database;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Province extends Model
+{
+  public $timestamps = false;
+  protected $table = 'city';
+  protected $primaryKey = 'city_id';
+  public $incrementing = false;
+
+  public function getAllProvince( $sort = null, $request = null )
+  {
+    $sort = $sort === null ? 'asc' : $sort;
+
+    if( $request === null )
+    {
+      return $this->orderBy('province_name', $sort)->get();
+    }
+    else
+    {
+      $keywords = $request->keywords;
+      $limit = $request->limit;
+
+      $query = $this->orderBy('province_name', $sort);
+
+      if( ! empty( $keywords ) )
+      {
+        $query = $query->where('province_name', 'like', '%' . $keywords . '%')
+        ->orWhere('province_id', 'like', '%' . $keywords . '%');
+      }
+
+      return $query->paginate( $limit );
+    }
+  }
+
+  public function getProvince( $id )
+  {
+    return $this->where( 'province_id', $id )->first();
+  }
+
+  public function addProvince( $request )
+  {
+    $province_name = $request->province_name;
+    $province_id = $request->province_id;
+    $check_id = $this->where('province_id', $province_id);
+    $res = [ 'statusCode' => 200, 'errorMessage' => '' ];
+
+    if( $check_id->count() == 1 )
+    {
+      $res = [ 'statusCode' => 409, 'errorMessage' => 'Duplicate ID. ID already exists!' ];
+    }
+    else
+    {
+      $this->province_id = $province_id;
+      $this->province_name = $province_name;
+      $this->save();
+    }
+
+    return $res;
+  }
+
+  public function updateProvince( $request, $id )
+  {
+    $province_name = $request->province_name;
+    $province_id = $request->province_id;
+    $res = [ 'statusCode' => 200, 'errorMessage' => '' ];
+
+    $getprovince = $this->getProvince( $id );
+    $getprovince->province_name = $province_name;
+
+    if( $getprovince->province_id == $province_id )
+    {
+      $getprovince->save();
+    }
+    else
+    {
+      $check_id = $this->where('province_id', $province_id);
+      if( $check_id->count() == 1 )
+      {
+        $res = [ 'statusCode' => 409, 'errorMessage' => 'Duplicate ID. ID already exists!' ];
+      }
+      else
+      {
+        $getprovince->province_id = $province_id;
+        $getprovince->save();
+      }
+    }
+
+    return $res;
+  }
+
+  public function deleteProvince( $id )
+  {
+    if( $id === null || empty( $id ) ) return false;
+    return $this->where('province_id', $id)->delete();
+  }
+}
