fe5205579c3faa405a9477062e978d5af71a9026
sonypiay <sonypiay@mail.com>
2019-11-02 12:10:27 +0700

    commit change

--- app/Database/ClientUser.php
+++ app/Database/ClientUser.php
@@ -13,16 +13,15 @@ class ClientUser extends Model
   protected $primaryKey = 'client_id';
   protected $guarded = ['created_at', 'updated_at'];
 
-  private $key = 'CUST';
-
   private function generateClientId()
   {
     $id = 1;
     $lastid = $this->orderBy('seqid', 'desc')->first();
     if( $lastid != null ) $id = $lastid->seqid + 1;
 
+    $key = 'CL';
     $pad = STR_PAD( $id, 4, '0', STR_PAD_LEFT );
-    return $this->key . $pad;
+    return $key . $pad;
   }
 
   public function getClientId()
@@ -97,5 +96,97 @@ class ClientUser extends Model
     }
 
     return $res;
+  }
+
+  public function saveProfie( $request )
+  {
+    $client_name = $request->client_name;
+    $client_email = $request->client_email;
+    $client_type = $request->client_type;
+    $client_gender = $request->client_gender;
+    $city = $request->city;
+    $client_id = session()->has('clientId') ? session()->get('clientId') : null;
+    $res = ['responseCode' => 200, 'responseMessage' => 'success'];
+
+    $getclient = $this->getProfile( $client_id );
+    $getclient->client_fullname = $client_name;
+    $getclient->client_type = $client_type;
+    $getclient->city_id = $city;
+    if( $client_type === 'individual' ) $getclient->client_gender = $client_gender;
+
+    if( $getclient->client_email == $client_email )
+    {
+      $getclient->save();
+    }
+    else
+    {
+      $check_email = $this->where('client_email', $client_id);
+      if( $check_email->count() == 1 )
+      {
+        $res = ['responseCode' => 409, 'responseMessage' => $client_email . ' already exists.'];
+      }
+      else
+      {
+        $getclient->client_email = $client_email;
+        $getclient->save();
+      }
+    }
+    return $res;
+  }
+
+  public function changePassword( $request )
+  {
+    $password = $request->password;
+    $hash_password = Hash::make( $password );
+    $res = ['responseCode' => 200, 'responseMessage' => 'success'];
+    $client_id = session()->has('clientId') ? session()->get('clientId') : null;
+
+    $getclient = $this->getProfile( $client_id );
+    $getclient->client_password = $hash_password;
+    $getclient->save();
+
+    return $res;
+  }
+
+  public function login( $request )
+  {
+    $client_email = $request->client_email;
+    $client_password = $request->client_password;
+    $client_id = session()->has('clientId') ? session()->get('clientId') : null;
+    $res = ['responseCode' => 200, 'responseMessage' => 'success'];
+
+    $validate = $this->where('client_email', $client_email);
+    if( $validate->count() == 0 )
+    {
+      $res = ['responseCode' => 401, 'responseMessage' => 'Account doesn\'t exists!'];
+    }
+    else
+    {
+      $result = $validate->first();
+      if( ! Hash::check( $client_password, $result->client_password ) )
+      {
+        $res = ['responseCode' => 401, 'responseMessage' => 'Password did not match!'];
+      }
+      else
+      {
+        $getclient = $this->getProfile( $client_id );
+        session()->put('isClient', true);
+        session()->put('clientId', $getclient->client_id);
+        session()->put('clientLogin', date('Y-m-d H:i:s'));
+        session()->put('clientIp', $request->server('REMOTE_ADDR'));
+      }
+    }
+
+    return $res;
+  }
+
+  public function logout()
+  {
+    session()->forget('isClient');
+    session()->forget('clientId');
+    session()->forget('clientLogin');
+    session()->forget('clientIp');
+
+    return session()->flush();
   }
 }
