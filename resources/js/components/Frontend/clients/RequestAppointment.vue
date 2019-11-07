d321d1c6dd7d8eaa59b89597c3d84b0fdd706af7
sonypiay <sonypiay@mail.com>
2019-11-07 21:17:39 +0700

    make appointment

--- resources/js/components/Frontend/clients/RequestAppointment.vue
+++ resources/js/components/Frontend/clients/RequestAppointment.vue
@@ -1,96 +1,5 @@
 <template>
   <div>
-
-    <div id="modal-edit-request" uk-modal>
-      <div class="uk-modal-dialog uk-modal-body modal-dialog">
-        <a class="uk-modal-close uk-modal-close-default" uk-close></a>
-        <div class="modal-title">Edit Request Appointment</div>
-        <div v-show="messages.successMessage" class="uk-margin-top uk-alert-success" uk-alert>
-          {{ messages.successMessage }}
-        </div>
-        <div v-show="messages.errorMessage" class="uk-margin-top uk-alert-danger" uk-alert>
-          {{ messages.errorMessage }}
-        </div>
-        <form class="uk-form-stacked uk-margin-top" @submit.prevent="onSaveRequest">
-          <div class="uk-margin">
-            <label class="uk-form-label gl-label">Select Date</label>
-            <div class="uk-form-controls">
-              <v-date-picker v-model="forms.selectedDate"
-              :min-date="datepicker.mindate"
-              :popover="datepicker.popover"
-              :columns="2"
-              >
-              <div class="uk-width-1-1 uk-inline">
-                <span class="uk-form-icon" uk-icon="calendar"></span>
-                <input type="text" class="uk-width-1-1 uk-input gl-input-default" :value="$root.formatDate( forms.selectedDate, 'ddd, DD MMMM YYYY' )" readonly />
-              </div>
-              </v-date-picker>
-            </div>
-          </div>
-          <div class="uk-margin">
-            <label class="uk-form-label gl-label">Select Time</label>
-            <div class="uk-form-controls">
-              <div class="uk-width-1-1 uk-inline">
-                <a class="uk-form-icon" uk-icon="clock"></a>
-                <input type="text" class="uk-width-1-1 uk-input gl-input-default"
-                v-model="selectedTime"
-                readonly />
-              </div>
-              <div class="uk-width-large dropdown-timepicker" uk-dropdown="mode: click;">
-                <div class="uk-dropdown-grid uk-child-width-1-2" uk-grid>
-                  <div>
-                    <div class="dropdown-timepicker-header">Hours</div>
-                    <div class="dropdown-timepicker-content">
-                      <ul class="uk-nav uk-nav-default uk-dropdown-nav nav-timepicker">
-                        <li>
-                          <a :class="{'active': $root.padNumber( 0, 2 ) == forms.timepicker.hours}" @click="onSelectedTime( 0, 'hours' )">
-                            {{ $root.padNumber( 0, 2 ) }}
-                          </a>
-                        </li>
-                        <li v-for="i in 23">
-                          <a :class="{'active': $root.padNumber( i, 2 ) == forms.timepicker.hours}" @click="onSelectedTime( i, 'hours' )">
-                            {{ $root.padNumber( i, 2 ) }}
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div>
-                    <div class="dropdown-timepicker-header">Minute</div>
-                    <div class="dropdown-timepicker-content">
-                      <ul class="uk-nav uk-nav-default uk-dropdown-nav nav-timepicker">
-                        <li>
-                          <a :class="{'active': $root.padNumber( 0, 2 ) == forms.timepicker.minute}" @click="onSelectedTime( 0, 'minute' )">
-                            {{ $root.padNumber( 0, 2 ) }}
-                          </a>
-                        </li>
-                        <li v-for="i in 59">
-                          <a :class="{'active': $root.padNumber( i, 2 ) == forms.timepicker.minute}" @click="onSelectedTime( i, 'minute' )">
-                            {{ $root.padNumber( i, 2 ) }}
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div v-show="messages.errors.timepicker" class="uk-text-small uk-text-danger">{{ messages.errors.timepicker }}</div>
-          </div>
-          <div class="uk-margin">
-            <label class="uk-form-label gl-label">Description</label>
-            <div class="uk-form-controls">
-              <textarea class="uk-textarea uk-height-small gl-input-default" v-model="forms.description" placeholder="Enter a description"></textarea>
-            </div>
-            <div v-show="messages.errors.description" class="uk-text-small uk-text-danger">{{ messages.errors.description }}</div>
-          </div>
-          <div class="uk-margin">
-            <button class="uk-button uk-button-default gl-button-default" v-html="forms.submit"></button>
-          </div>
-        </form>
-      </div>
-    </div>
-
     <div class="navbar-event">
       <div class="uk-container">
         <nav class="uk-navbar">
@@ -183,6 +92,9 @@ export default {
           next_page_url: ''
         },
         details: {}
+      },
+      messages: {
+        errorMessage: ''
       }
     }
   },
